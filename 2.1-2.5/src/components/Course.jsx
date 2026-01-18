c6c148608fe72f158fca8c9a227ef0c252c53d48
NassarBarahme <nassarbarahme@gmail.com>
2026-01-18 19:58:24 +0200

    Fix: remove embedded git repo from 2.1-2.5

---
+++ 2.1-2.5/src/components/Course.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+
+const Header = ({ course }) => <h1>{course.name}</h1>;
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+
+const Content = ({ course }) => {
+  return (
+    <>
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </>
+  );
+};
+
+
+const Total = ({ course }) => {
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return <p><b>total of {total} exercises</b></p>;
+};
+
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+export default Course;
